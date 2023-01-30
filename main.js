712b243507d9b6f8edaeef080a6f63f83b1cbb88
Maverick <sc20ml@leeds.ac.uk>
2023-01-30 16:56:30 +0000

    Created chess board

--- main.js
+++ main.js
@@ -9,16 +9,18 @@ const camera = new THREE.PerspectiveCamera(75, aspectRatio , 0.1, 1000);
 const renderer = new THREE.WebGLRenderer(
   {canvas: document.querySelector('#bg'),}
 );
+// const background = new THREE.TextureLoader().load('textures/rubber-fig-gray-room.jpg');
+// scene.background = background;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 renderer.render(scene, camera);
 
-// Creating an object
-const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347, wireframe: true});
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+// Chess board properties
+const tileGeometry = new THREE.BoxGeometry(1, 0.1, 1);
+const lightTile = new THREE.MeshBasicMaterial({color: 0xe3d8bd});
+const darkTile = new THREE.MeshBasicMaterial({color: 0x77593e});
+scene.add(create_board());
 
 // Create light source
 const pointLight = new THREE.PointLight(0xffffff);
@@ -34,12 +36,27 @@ scene.add(lightHelper, gridHelper)
 // Create controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function create_board() {
+  const board = new THREE.Group();
+  for(let x = 0; x < 8; x++) {
+    for(let z = 0; z < 8; z++) {
+      if (z % 2 == false) {
+        var tile;
+        tile = new THREE.Mesh(tileGeometry, x % 2 == false? lightTile: darkTile);
+      }
+      else {
+        tile = new THREE.Mesh(tileGeometry, x % 2 == false? darkTile: lightTile);
+      }
+      tile.position.set(x-3.5, 0, z-3.5);
+      board.add(tile);
+    }
+  }
+  return board;
+}
+
 function animate() {
   requestAnimationFrame(animate);
   
-  // Animate torus
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.01;
   renderer.render(scene, camera)
 
   // Update controls every frame
